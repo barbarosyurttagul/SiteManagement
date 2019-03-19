1861f8d6dc6d98b297fb4ac332459f8683dd0a38
Barbaros Yurttagül <barbaros.yurttagul@gmail.com>
2019-03-19 23:22:52 +0000

    NHibernate SqlServerHelper Implementation

---
+++ Barbarosoft.SiteManagement.DataAccess/Concrete/NHibernate/Helpers/SqlServerHelper.cs
@@ -0,0 +1,26 @@
+﻿using Barbarosoft.Core.DataAccess.NHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Barbarosoft.SiteManagement.DataAccess.Concrete.NHibernate.Helpers
+{
+    public class SqlServerHelper : NHibernateHelper
+    {
+        protected override ISessionFactory InitializeFactory()
+        {
+            //We used Fluently to configure NHibernate instead of XML
+            return Fluently
+                .Configure()
+                .Database(MsSqlConfiguration
+                    .MsSql2012
+                    .ConnectionString("Data Source=DESKTOP-1AC67OU\\SQLEXPRESS; Database=SiteManagementDb;Integrated Security=True"))
+                .Mappings(t => t.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
+                .BuildSessionFactory();
+        }
+    }
+}
