8e918f94004e731a56dad123fc38678db18e627c
Barbaros Yurttagül <barbaros.yurttagul@gmail.com>
2019-03-19 23:59:38 +0000

    NHibernate CurrentOccupierDal Implementation

---
+++ Barbarosoft.SiteManagement.DataAccess/Concrete/NHibernate/NhCurrentOccupierDal.cs
@@ -0,0 +1,16 @@
+﻿using Barbarosoft.Core.DataAccess.NHibernate;
+using Barbarosoft.SiteManagement.DataAccess.Abstract;
+using Barbarosoft.SiteManagement.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barbarosoft.SiteManagement.DataAccess.Concrete.NHibernate
+{
+    public class NhCurrentOccupierDal : NhEntityRepositoryBase<CurrentOccupier>, ICurrentOccupierDal
+    {
+        public NhCurrentOccupierDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+        }
+    }
+}
