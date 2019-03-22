8b1a6998585abff97bb3684db89b9e60cfb5ab87
Barbaros Yurttagül <barbaros.yurttagul@gmail.com>
2019-03-22 15:15:51 +0000

    CurrentOccupier Validation

---
+++ Barbarosoft.SiteManagement.Business/ValidationRules/FluentValidation/CurrentOccupierValidator.cs
@@ -0,0 +1,18 @@
+﻿using Barbarosoft.SiteManagement.Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barbarosoft.SiteManagement.Business.ValidationRules.FluentValidation
+{
+    public class CurrentOccupierValidator : AbstractValidator<CurrentOccupier>
+    {
+        public CurrentOccupierValidator()
+        {
+            RuleFor(x => x.FirstMidName).NotEmpty();
+            RuleFor(x => x.CheckinDate).NotEmpty();
+            RuleFor(x => x.FlatId).NotEmpty();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 "# SiteManagement" 
+
+# Libraries
+- Barbarosoft.Core
+* EntityFramework.Core 2.2.3
+* NHibernate 5.2.4
+- Barbarosoft.SiteManagement.Business
+* FluentValidation.AspNetCore 8.1.3
+- Barbarosoft.SiteManagement.DataAccess
+* FluentNHibernate 2.1.2 (for Nhibernate mapping)
+* EntityFrameworkCore.SqlServer 2.2.3
+
+
