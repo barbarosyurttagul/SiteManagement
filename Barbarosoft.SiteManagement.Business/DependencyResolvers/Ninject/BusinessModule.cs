06d034d172b60bd8df90bc3ba24e9a56517fa900
Barbaros Yurttagül <barbaros.yurttagul@gmail.com>
2019-04-16 09:14:05 +0100

    Ninject Implementation

---
+++ Barbarosoft.SiteManagement.Business/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Barbarosoft.SiteManagement.Business.Abstract;
+using Barbarosoft.SiteManagement.Business.Concrete.Managers;
+using Barbarosoft.SiteManagement.DataAccess.Abstract;
+using Barbarosoft.SiteManagement.DataAccess.Concrete.EntityFramework;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barbarosoft.SiteManagement.Business.DependencyResolvers.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ICurrentOccupierService>().To<CurrentOccupierManager>().InSingletonScope();
+            Bind<ICurrentOccupierDal>().To<EfCurrentOccupierDal>();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 * Postsharp 6.1.17
 # Barbarosoft.SiteManagement.Business
 * FluentValidation.AspNetCore 8.1.3
+* Postsharp 6.1.17
+* Ninject 3.3.4
 # Barbarosoft.SiteManagement.DataAccess
 * FluentNHibernate 2.1.2 (for Nhibernate mapping)
 * EntityFrameworkCore.SqlServer 2.2.3
