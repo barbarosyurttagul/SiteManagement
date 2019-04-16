53dfaae630ae596c1597738be5cd5ebff14ae8d3
Barbaros Yurttagül <barbaros.yurttagul@gmail.com>
2019-04-16 15:01:23 +0100

    Transaction Aspect

---
+++ Barbarosoft.Core/Aspects/Postsharp/TransactionAspects/TransactionScopeAspect.cs
@@ -0,0 +1,40 @@
+﻿using PostSharp.Aspects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace Barbarosoft.Core.Aspects.Postsharp.TransactionAspects
+{
+    [Serializable]
+    public class TransactionScopeAspect : OnMethodBoundaryAspect
+    {
+        private readonly TransactionScopeOption _option;
+
+        public TransactionScopeAspect()
+        {
+
+        }
+
+        public TransactionScopeAspect(TransactionScopeOption option)
+        {
+            _option = option;
+        }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            args.MethodExecutionTag = new TransactionScope(_option);
+        }
+
+        public override void OnSuccess(MethodExecutionArgs args)
+        {
+            ((TransactionScope)args.MethodExecutionTag).Complete();
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            ((TransactionScope)args.MethodExecutionTag).Dispose();
+        }
+
+    }
+}
